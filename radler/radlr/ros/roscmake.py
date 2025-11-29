9347ee156a846e62dceb43c3c670b97ee2eade41
Karthik Nukala <karthik.nukala@sri.com>
2025-11-29 12:07:13 -0800

    roscmake.py C++ compatibility

--- radler/radlr/ros/roscmake.py
+++ radler/radlr/ros/roscmake.py
@@ -57,9 +57,9 @@ def clear(d, templates):
 
 cmake_templates = {
 'cmakeliststxt': """
-cmake_minimum_required(VERSION 3.30)
+cmake_minimum_required(VERSION 3.16)
 
-set(CMAKE_CXX_STANDARD 23)
+set(CMAKE_CXX_STANDARD 17)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 
 project({module})
